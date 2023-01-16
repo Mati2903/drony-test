3aaa16cba8cf4a282fc51e6905ba533e1a369036
Mati2903 <mazurek321.dev@gmail.com>
2023-01-16 23:02:49 +0100

    change code for animations to work depending on image id

--- components/elements/TextAndImage.js
+++ components/elements/TextAndImage.js
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
 import { useEffect, useState } from "react";
 
-const TextAndImage = ({ title, paragraph, imgUrl, imgAlt, direction }) => {
+const TextAndImage = ({ title, paragraph, imgUrl, imgAlt, direction, id }) => {
 	const [scrollY, setScrollY] = useState(0);
 	const [ref, inView] = useInView({
 		threshold: 0, // amount of image visible to start animation
@@ -17,21 +17,54 @@ const TextAndImage = ({ title, paragraph, imgUrl, imgAlt, direction }) => {
 		return () => window.removeEventListener("scroll", handleScroll);
 	}, []);
 
+	let animation;
+	switch (id) {
+		case 1:
+			animation = {
+				y: inView ? scrollY / 30 : 0,
+				rotate: inView ? scrollY / 100 : 0,
+			};
+			break;
+		case 2:
+			animation = {
+				y: inView ? scrollY / 30 : 0,
+				rotate: inView ? -scrollY / 100 : 0,
+			};
+			break;
+		case 3:
+			animation = {
+				y: inView ? scrollY / 30 : 0,
+				rotate: inView ? scrollY / 100 : 0,
+			};
+			break;
+		case 4:
+			animation = {
+				y: inView ? scrollY / 30 : 0,
+				rotate: inView ? scrollY / 100 : 0,
+			};
+			break;
+		default:
+			animation = { y: 0, rotate: 0 };
+	}
+
 	return (
-		<div className="text-image-container" style={{ flexDirection: direction }}>
+		<div
+			ref={ref}
+			className="text-image-container"
+			style={{ flexDirection: direction }}
+		>
 			<div className="text-container">
 				<h2 className="text-container__title">{title}</h2>
 				<p className="text-container__paragraph">{paragraph}</p>
 			</div>
-			<div ref={ref} className="img-container">
+			<div className="img-container">
 				<motion.img
 					src={imgUrl}
 					alt={imgAlt}
-					initial={{ rotate: -5 }}
-					animate={{
-						rotate: inView ? scrollY / 150 : -5,
-					}}
+					initial={{ y: 0, rotate: 0 }}
+					animate={animation}
 					transition={{ duration: 0 }}
+					positionTransition //property for remember last position after animation
 				/>
 			</div>
 		</div>
--- components/sections/OurDrones.js
+++ components/sections/OurDrones.js
@@ -8,6 +8,7 @@ const OurDrones = () => {
 				paragraph="Jest to dron, który pozwala na nagrywanie niezwykle dynamicznych scen, głównie na wolnym powietrzu, wykonując w locie różnego rodzaju akrobacje lub latając za samochodami czy też motocyklami. Można nim wykonać również widokowe ujęcia w pobliżu budynków lub innych dużych obiektów."
 				imgUrl="/dron-5-freestyle.png"
 				imgAlt="zdjęcie drona typu FPV 5 cali freestyle"
+				id={1}
 			/>
 			<TextAndImage
 				title="Dron 3,5” cinewhoop"
@@ -15,12 +16,22 @@ const OurDrones = () => {
 				imgUrl="/dron-3,5-cinewhoop.png"
 				imgAlt="zdjęcie drona typu FPV 3,5 cala cinewhoop"
 				direction="row-reverse"
+				id={2}
 			/>
 			<TextAndImage
 				title="Dron 2,5” mini cinewchoop"
 				paragraph="Ten typ drona jest idealny do latania i filmowania wewnątrz pomieszczeń, w pobliżu drogich przedmiotów czy też latając bardzo blisko ludzi. Wykonuje się nim też przeloty przez całe obiekty, a nawet wnętrza samochodów. Jego niewielkie gabaryty i waga powodują, że jest on bardzo precyzyjny."
 				imgUrl="/dron-2,5-mini-cinewhoop.png"
 				imgAlt="zdjęcie drona typu FPV 2,5 cala mini cinewhoop"
+				id={3}
+			/>
+			<TextAndImage
+				title="Urządzenia nagrywające"
+				paragraph="Kamery zamontowane na dronach to GoPro 11 oraz GoPro 10. Umożliwiają one zapis obrazu w formacie 5,3k - 4k – 2,7k w dowolnym klatkażu oraz w 10 bitowym systemie kolorów."
+				imgUrl="/gopro11.png"
+				imgAlt="zdjęcie kamery gopro 11"
+				direction="row-reverse"
+				id={4}
 			/>
 		</section>
 	);
