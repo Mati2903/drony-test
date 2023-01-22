c1b23fccd03b15c3509e74122e787fbd7310d00f
Mati2903 <mazurek321.dev@gmail.com>
2023-01-22 11:39:30 +0100

    add WhatIsFPV section with functionality of setting top value based on previous sections heights and listener for window resize event

--- components/sections/WhatIsFPV.js
+++ components/sections/WhatIsFPV.js
@@ -1,9 +1,30 @@
 import Image from "next/image";
 import TextAndImage from "../elements/TextAndImage";
+import { useState, useEffect } from "react";
 
 const WhatIsFPV = () => {
+	const [elementOffsetBottom, setElementOffsetBottom] = useState(0);
+
+	//set top value for section based on above sections heights
+	useEffect(() => {
+		const handleResize = () => {
+			setElementOffsetBottom(
+				document.querySelector(".our-drones").offsetTop +
+					document.querySelector(".our-drones").offsetHeight -
+					document.querySelector(".nav").offsetHeight
+			);
+		};
+		//listener for window resize event, e.g. when user is changing view from portrait to landscape
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, [elementOffsetBottom]);
+
 	return (
-		<section id="what-is-fpv" className="what-is-fpv">
+		<section
+			id="what-is-fpv"
+			className="what-is-fpv"
+			style={{ top: elementOffsetBottom }}
+		>
 			<div></div>
 		</section>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Nav from "../components/sections/Nav";
 import Video from "../components/sections/Video";
 import OurDrones from "../components/sections/OurDrones";
+import WhatIsFPV from "../components/sections/WhatIsFPV";
 
 export default function Home() {
 	return (
@@ -8,6 +9,7 @@ export default function Home() {
 			<Nav />
 			<Video />
 			<OurDrones />
+			<WhatIsFPV />
 		</>
 	);
 }
